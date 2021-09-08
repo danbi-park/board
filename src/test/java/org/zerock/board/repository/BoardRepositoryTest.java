5d4fc7e6ab7bd9487b5f3d8f036503e1e9a025ee
sweg-db <bhd03067@naver.com>
2021-09-09 01:56:13 +0900

    연관관계 - ManyToOne, Eager/Lazy loading,

--- src/test/java/org/zerock/board/repository/BoardRepositoryTest.java
+++ src/test/java/org/zerock/board/repository/BoardRepositoryTest.java
@@ -3,12 +3,11 @@ package org.zerock.board.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.zerock.board.entity.Board;
-import org.zerock.board.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
-import java.util.stream.IntStream;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
 
 @SpringBootTest
 class BoardRepositoryTest {
@@ -16,7 +15,8 @@ class BoardRepositoryTest {
     @Autowired
     private BoardRepository boardRepository;
 
-    @Test
+
+/*    @Test
     public void insertBoard(){
 
         IntStream.rangeClosed(1,100).forEach(i -> {
@@ -28,6 +28,47 @@ class BoardRepositoryTest {
                     .writer(member)
                     .build();
             boardRepository.save(board);
+        });
+    }*/
+
+/*    @Transactional
+    @Test
+    public void testRead1(){
+        Optional<Board> result = boardRepository.findById(100L);
+        if (result.isPresent()){
+            Board board = result.get();
+            System.out.println(board);
+            System.out.println(board.getWriter());
+        }
+    }*/
+
+
+/*    @Test
+    public void testReadWithWriter(){
+        Object result = boardRepository.getBoardWithWriter(100L);
+        Object[] arr = (Object[]) result;
+        System.out.println("-------------------------");
+        System.out.println(Arrays.toString(arr));
+    }*/
+
+/*    @Test
+    public void testReadWithReply(){
+        List<Object[]> result = boardRepository.getBoardWithWriter(100L);
+
+        for (Object[] arr : result){
+            System.out.println(Arrays.toString(arr));
+        }
+    }*/
+
+    //목록화면
+    @Test
+    public void testWithReplyCount(){
+        PageRequest pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+        //개수 구하기
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+        result.get().forEach(row -> {
+            Object[] arr = (Object[]) row;
+            System.out.println(Arrays.toString(arr));
         });
     }
 }
--- src/test/java/org/zerock/board/repository/ReplyRepositoryTest.java
+++ src/test/java/org/zerock/board/repository/ReplyRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Reply;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,7 +17,7 @@ class ReplyRepositoryTest {
     @Autowired
     private ReplyRepository replyRepository;
 
-    @Test
+    /*@Test
     public void insertReply(){
 
         IntStream.rangeClosed(1,300).forEach(i -> {
@@ -30,7 +31,18 @@ class ReplyRepositoryTest {
 
             replyRepository.save(reply);
         });
-    }
+    }*/
+
+/*    @Test
+    public void readReply1(){
+        Optional<Reply> result = replyRepository.findById(1L);
+        if (result.isPresent()){
+            Reply reply = result.get();
+            System.out.println(reply);
+            System.out.println(reply.getBoard());
+        }
+    }*/
+
 
 
 }
